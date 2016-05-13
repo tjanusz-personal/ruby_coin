4013bd10e218a2eb59d4860e9219fd7d5ccdd889
Tim Janusz <TJanusz@comcast.net>
2016-05-13 13:03:34 -0400

    Updating to include new additions

--- pull_kennedy.rb
+++ pull_kennedy.rb
@@ -6,9 +6,9 @@ class PullKennedy
   include Celluloid
 
   @@item_filter_hash = { :ListingType => ["Auction", "AuctionWithBIN"] }
-  @@years_needed = ["1971", "1973", "1974", "1978", "1981", "1982", "1983", "1984", "1997"];
+  @@years_needed = ["1971", "1973", "1974", "1978", "1981", "1982", "1983", "1984", "1997", "2016"];
   @@skip_words = ["1971-D", "1974-D", "1974 D", "1973-D", "1973 D", "1980-P", "1983-D", "1984 P", "1981S", "1981 P",
-    "1997-P", "1971 D", "1982-D", "1982 D", "1981 P", "1981-P"];
+    "1997-P", "1971 D", "1981 P", "1981-P", "1982-D", "1982 D"];
   @@coin_type = "Kennedy"
   @@aspect_filters = [ {:Certification => "NGC"}, {:Certification => "PCGS"}, {:Grade => "MS 66"}]
 
@@ -17,7 +17,7 @@ class PullKennedy
     response = ebay_utils.do_ebay_query(app_id, "Kennedy Half Dollar", @@aspect_filters, @@item_filter_hash, page_number)
     results_processor = ResultsProcessor.new
     filtered_results = results_processor.filter_results(@@coin_type, response, @@years_needed, @@skip_words, 50)
-    # puts filtered_results
+    #puts filtered_results
     filtered_results
   end
 
