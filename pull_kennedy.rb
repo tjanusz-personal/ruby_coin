9ec9069e957bbb0b5c2796e3c9658bbe9847627d
Tim Janusz <TJanusz@comcast.net>
2016-11-07 21:09:43 -0500

    updating with latest purchases

--- pull_kennedy.rb
+++ pull_kennedy.rb
@@ -6,9 +6,9 @@ class PullKennedy
   include Celluloid
 
   @@item_filter_hash = { :ListingType => ["Auction", "AuctionWithBIN"] }
-  @@years_needed = ["1971", "1974", "1978", "1981", "1982", "1983", "1984"];
-  @@skip_words = ["1971-D", "1974-D", "1974 D", "1973-D", "1973 D", "1980-P", "1983-D", "1984 P", "1981S", "1981 P",
-    "1997-P", "1971 D", "1981 P", "1981-P", "1982-D", "1982 D"];
+  @@years_needed = ["1968", "1971", "1974", "1978", "1981", "1982", "1984"];
+  @@skip_words = ["1971-D", "1974-D", "1974 D", "1984 P", "1981S", "1981 P",
+    "1971 D", "1981 P", "1981-P", "1982-D", "1982 D"];
   @@coin_type = "Kennedy"
   @@aspect_filters = [ {:Certification => "NGC"}, {:Certification => "PCGS"}, {:Grade => "MS 66"}]
 
--- pull_kennedy_bin.rb
+++ pull_kennedy_bin.rb
@@ -6,10 +6,10 @@ class PullKennedyBIN
   include Celluloid
 
   @@item_filter_hash = { :ListingType => ["FixedPrice"] }
-  @@years_needed = ["1971", "1974", "1978", "1981", "1982", "1983", "1984"]
-  @@skip_words = ["1971 D", "1971-D", "1974 D", "1974-D", "1973 D", "1973-D", "2008 P", "2008-P", "1980-P",
-    "1971 S", "1978-P", "1983-D", "1983 D", "1981-P","1984-P", "1984 P", "1978 P",
-    "1974-D", "1997 P", "1980 P", "1978 KENN", "1981S", "1981 P", "1997-P", "1982-D", "1982 D"]
+  @@years_needed = ["1971", "1974", "1978", "1981", "1982", "1984"]
+  @@skip_words = ["1971 D", "1971-D", "1974 D", "1974-D",
+    "1971 S", "1978-P", "1981-P","1984-P", "1984 P", "1978 P",
+    "1974-D", "1978 KENN", "1981S", "1981 P", "1982-D", "1982 D"]
   @@coin_type = "KennedyBIN"
   @@aspect_filters = [ {:Certification => "NGC"}, {:Certification => "PCGS"}, {:Grade => "MS 66"}]
 
